606f3b06e4eb404281ce041faff3c801e2186e88
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-13 17:44:41 +0530

    added javascrip revealing section when scrolled

---
+++ index.js
@@ -0,0 +1,16 @@
+// Revealing Section when Scroll
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer){
+    const [entry] = entries;
+    console.log(entry);
+    if(!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold: 0.15});
+
+allSection.forEach((section)=>{
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+})
