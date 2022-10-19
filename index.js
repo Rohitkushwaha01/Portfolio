b490e62ba909a119a1d20892944b66ee48a1b620
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-20 00:03:23 +0530

    info

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Revealing Section when Scroll
-// const allSection = document.querySelectorAll('.section');
+const allSection = document.querySelectorAll('.section');
 
-// const revealSection = function(entries, observer){
-//     const [entry] = entries;
-//     if(!entry.isIntersecting) return;
-//     entry.target.classList.remove('section--hidden');
-//     observer.unobserve(entry.target);
-// }
+const revealSection = function(entries, observer){
+    const [entry] = entries;
+    if(!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+}
 
-// const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold: 0.15});
+const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold: 0.15});
 
-// allSection.forEach((section)=>{
-//     sectionObserver.observe(section);
-//     section.classList.add('section--hidden');
-// })
+allSection.forEach((section)=>{
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+})
